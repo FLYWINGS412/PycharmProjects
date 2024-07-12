f6b88a149ee462d0ad8ffdeeae6790cca053b0c5
63061 <flywings412@gmail.com>
2024-07-13 00:53:18 +0800

    更新了自动化

--- xiangshi/utils/utils.py
+++ xiangshi/utils/utils.py
@@ -99,13 +99,13 @@ def get_elements(driver, by, value):
 def get_elements_with_uiautomator(driver, uiautomator_string):
     try:
         # 使用 Android UI Automator 直接查找元素
-        elements = WebDriverWait(driver, 10).until(
-            EC.visibility_of_all_elements_located((MobileBy.ANDROID_UIAUTOMATOR, uiautomator_string))
+        elements = WebDriverWait(driver, 0).until(
+            EC.presence_of_all_elements_located((MobileBy.ANDROID_UIAUTOMATOR, uiautomator_string))
         )
         # print(f"找到 {len(elements)} 个可见元素使用 UI Automator: {uiautomator_string}")  # 添加调试信息
         return elements
     except TimeoutException:
-        print("在指定时间内没有找到元素")  # 添加调试信息
+        # print("在指定时间内没有找到元素")  # 添加调试信息
         return []
 
 # 获取关闭按钮
