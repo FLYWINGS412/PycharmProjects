06191c05d5768ec5ae750e5a6803f26714fb70bb
FLYWINGS412 <flywings412@gmail.com>
2024-07-17 17:30:19 +0800

    更新了自动化

--- xiangshi/tasks/tasks.py
+++ xiangshi/tasks/tasks.py
@@ -165,42 +165,46 @@ def mutual_assistance_reward(driver, account):
     while True:
         start_time = time.time()  # 记录循环开始时间
 
-        # 激励视频奖励
-        reward_layer = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((MobileBy.ID, "com.xiangshi.bjxsgc:id/txt_reward_ad"))
-        )
-        time.sleep(random.randint(2, 5))
-        reward_layer.click()
-        print("点击了激励视频奖励")
+        # 检查激励视频奖励
+        try:
+            # 激励视频奖励
+            reward_layer = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((MobileBy.ID, "com.xiangshi.bjxsgc:id/txt_reward_ad"))
+            )
+            time.sleep(random.randint(2, 5))
+            reward_layer.click()
+            print("点击了激励视频奖励")
 
-        # 等待页面完成加载
-        time.sleep(1)
-        WebDriverWait(driver, 60).until(
-            EC.invisibility_of_element_located((MobileBy.ID, "com.xiangshi.bjxsgc:id/text"))
-        )
+            # 等待页面完成加载
+            time.sleep(1)
+            WebDriverWait(driver, 60).until(
+                EC.invisibility_of_element_located((MobileBy.ID, "com.xiangshi.bjxsgc:id/text"))
+            )
 
-        if not utils.is_on_ad_page(driver):
-            print("加载展示页。")
-            if not handle_display_page(driver):
-                return False
+            if not utils.is_on_ad_page(driver):
+                print("加载展示页。")
+                if not handle_display_page(driver):
+                    return False
 
-            # 检查是否存在包含“每日”文本的元素
-            try:
-                WebDriverWait(driver, 3).until(
-                    EC.presence_of_element_located((MobileBy.XPATH, "//*[contains(@text, '每日20次')]"))
-                )
-                print("检查到'每日20次'文本，程序终止并退出到系统桌面。")
-                utils.log_mutual_assistance_reward(driver, account)
-                break
-            except TimeoutException:
-                print("未检查到'每日20次'文本，继续执行。")
-                counter += 1
-                if counter >= max_attempts:
-                    print("达到最大尝试次数，退出循环。")
+                # 检查是否存在包含“每日”文本的元素
+                try:
+                    WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located((MobileBy.XPATH, "//*[contains(@text, '每日20次')]"))
+                    )
+                    print("检查到'每日20次'文本，程序终止并退出到系统桌面。")
                     utils.log_mutual_assistance_reward(driver, account)
                     break
-        else:
-            print("没有加载展示页，继续执行滑动操作。")
+                except TimeoutException:
+                    print("未检查到'每日20次'文本，继续执行。")
+                    counter += 1
+                    if counter >= max_attempts:
+                        print("达到最大尝试次数，退出循环。")
+                        utils.log_mutual_assistance_reward(driver, account)
+                        break
+            else:
+                print("没有加载展示页，继续执行滑动操作。")
+        except Exception as e:
+            print("未找到或不可点击激励广告。")
 
         # 首页红包奖励
         if not popups.home_video_bonus(driver):
@@ -353,7 +357,6 @@ def handle_display_page(driver):
         for element in elements:
             location = element.location
             size = element.size
-
             if location['x'] < driver.width * 0.15 and location['y'] < driver.height * 0.15 and size['height'] < 70 and size['width'] < 70:
                 # print(f"找到左上角的 RelativeLayout, 位置: {location}, 大小: {size}")
                 print(f"找到左上角的 RelativeLayout")
@@ -361,8 +364,9 @@ def handle_display_page(driver):
                 # 进入直播间
                 return browse_live_room(driver)
     except TimeoutException:
-        print("不在直播间")
-        # pass  # 如果未找到元素，继续执行后续代码
+        pass  # 忽略超时异常
+    except StaleElementReferenceException:
+        pass  # 忽略元素过期异常
 
     element_to_wait = None  # 初始化 element_to_wait 为 None
     event = threading.Event()  # 用于同步倒计时结束的事件
